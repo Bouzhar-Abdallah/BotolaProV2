6c300d74ae9ee25bb5ff1e25b318be4fc1814b0d
abdallah <abddoullah.bouzhar@gmail.com>
2024-04-02 11:23:10 +0000

    B2-6

---
+++ api/src/main/java/Bouzhar/BotolaPro/demo/config/ConfigurationGeneral.java
@@ -0,0 +1,13 @@
+package Bouzhar.BotolaPro.demo.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationGeneral {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
