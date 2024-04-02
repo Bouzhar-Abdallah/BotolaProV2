611963e9d600135aff44cb6af6590dac0ca9c801
abdallah <abddoullah.bouzhar@gmail.com>
2024-04-02 11:32:45 +0000

    B2-13

---
+++ front/src/app/models/article.ts
@@ -0,0 +1,8 @@
+export interface Article{
+    id: number;
+    cover_url: string;
+    likes: number;
+    title: string;
+    content: string;
+}
+
---
+++ front/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { routes } from '../routing/routes';
+import { DialogContentComponent } from './dialog-content/dialog-content.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports:[
+    NavbarComponent,
+    
+  ]
+})
+export class SharedModule { }
