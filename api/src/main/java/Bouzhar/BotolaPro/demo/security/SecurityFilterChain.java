4ca9d9824b995f9de2a15c7d06c269e35c97cbab
abddoullah.bouzhar <abddoullah.bouzhar@gmail.com>
2024-04-02 14:31:07 +0000

    Configuration class for Spring Security settings. This class configures the security aspects of the application, including CORS settings, CSRF protection, session management, authentication providers, and JWT filter integration. It also defines the security filter chain that specifies which requests are secured and how.

---
+++ api/src/main/java/Bouzhar/BotolaPro/demo/security/SecurityFilterChain.java
@@ -0,0 +1,133 @@
+package Bouzhar.BotolaPro.demo.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.IF_REQUIRED;
+
+/**
+ * Configuration class for Spring Security settings.
+ * This class configures the security aspects of the application, including CORS settings,
+ * CSRF protection, session management, authentication providers, and JWT filter integration.
+ * It also defines the security filter chain that specifies which requests are secured and how.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityFilterChain {
+
+    private static final List<String> WHITE_LIST_URL = List.of(
+            "/api/v2/**",
+            "/api/v2/auth/**",
+            "/api/v2/Oauth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html",
+            "/actuator/**"
+    );
+    private final LogoutHandler logoutHandler;
+    private final CsrfCookieFilter csrfCookieFilter;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final CorsConfigurationSource corsConfigurationSource;
+    private final CsrfTokenRepository csrfTokenRepository;
+    private final SpaCsrfTokenRequestHandler spaCsrfTokenRequestHandler;
+
+    /**
+     * Configures the security filter chain for the application.
+     * This method sets up CORS, CSRF protection, session management, and configures authentication
+     * and authorization for HTTP requests. It also integrates the JWT authentication filter and
+     * defines logout behavior.
+     *
+     * @param http HttpSecurity object to configure security settings.
+     * @return SecurityFilterChain object representing the configured security filter chain.
+     * @throws Exception if an error occurs during the configuration process.
+     */
+    @Bean
+    public org.springframework.security.web.SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource)
+                )
+                .csrf(csrf ->
+                        csrf.csrfTokenRepository(csrfTokenRepository)
+                                .ignoringRequestMatchers("/h2-console/**")
+                                .csrfTokenRequestHandler(spaCsrfTokenRequestHandler)
+                )
+                .exceptionHandling(customizer ->
+                        customizer.authenticationEntryPoint(
+                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                        )
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(IF_REQUIRED)
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                        .httpStrictTransportSecurity(hsts -> hsts
+                                .includeSubDomains(true)
+                                .maxAgeInSeconds(31536000)
+                        )
+                )
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(createWhiteListMatchers())
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(
+                        jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .addFilterAfter(
+                        csrfCookieFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .logout(logout ->
+                        logout.logoutUrl("/api/v2/auth/logout")
+                                .addLogoutHandler(logoutHandler)
+                                .logoutSuccessHandler((request, response, authentication) -> {
+                                    response.setStatus(HttpStatus.OK.value());
+                                })
+                );
+        return http.build();
+    }
+
+    /**
+     * Generates an array of AntPathRequestMatcher objects based on a list of URL patterns.
+     * These matchers are used to define which paths are exempt from authentication.
+     *
+     * @return An array of AntPathRequestMatcher objects.
+     */
+    private AntPathRequestMatcher[] createWhiteListMatchers() {
+        return WHITE_LIST_URL.stream()
+                .map(AntPathRequestMatcher::new)
+                .toArray(AntPathRequestMatcher[]::new);
+    }
+}
