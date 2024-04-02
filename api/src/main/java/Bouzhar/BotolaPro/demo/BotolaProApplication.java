890ef5364ae975ad40259207f4d2472779d53863
abdallah <abddoullah.bouzhar@gmail.com>
2024-04-02 11:31:59 +0000

    B2-10

---
+++ api/src/main/java/Bouzhar/BotolaPro/demo/BotolaProApplication.java
@@ -0,0 +1,13 @@
+package Bouzhar.BotolaPro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotolaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotolaProApplication.class, args);
+	}
+
+}
---
+++ api/src/main/java/Bouzhar/BotolaPro/demo/config/MapStructConfig.java
@@ -0,0 +1,8 @@
+package Bouzhar.BotolaPro.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@MapperScan("Bouzhar.BotolaPro.demo.mapping")
+public class MapStructConfig {
+}
